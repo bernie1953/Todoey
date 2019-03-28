f7c88d2d71441c7350981ca6d73f4795fa6c97a8
bernie1953 <b.maire@yahoo.fr>
2019-03-28 17:27:32 +0100

    Commit après Time Machine

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -16,7 +16,6 @@ class TodoListViewController: UITableViewController {
         
     }
     
-    
     //MARK - Tableview Datasource Methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -28,21 +27,6 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         
         return cell
-    }
-    
-    //MARK - Tableview Delegate Methods
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
-        
-        
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
-        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
